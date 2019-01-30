fb33c7d5e8d1a49cce16d6dce0c2cc33f63bea91
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-01-30 14:48:18 +0700

    BC2_SS8_Bai1

--- TennisGame.php
+++ TennisGame.php
@@ -10,66 +10,77 @@ class TennisGame
 {
     public $score = '';
 
-    public function getScore($player1Name, $player2Name, $m_score1, $m_score2)
+    public function getScore($playerOneScore, $playerTwoScore)
     {
-        $tempScore=0;
-
-        if ($m_score1==$m_score2) {
-            switch ($m_score1)
-            {
-                case 0:
-                    $this->score = "Love-All";
-                    break;
-                case 1:
-                    $this->score = "Fifteen-All";
-                    break;
-                case 2:
-                    $this->score = "Thirty-All";
-                    break;
-                case 3:
-                    $this->score = "Forty-All";
-                    break;
-                default:
-                    $this->score = "Deuce";
-                    break;
-
-            }
-        }
-        else if ($m_score1>=4 || $m_score2>=4)
-        {
-            $minusResult = $m_score1-$m_score2;
-            if ($minusResult==1) $this->score ="Advantage player1";
-            else if ($minusResult ==-1) $this->score ="Advantage player2";
-            else if ($minusResult>=2) $this->score = "Win for player1";
-            else $this->score ="Win for player2";
-        }
-        else
-        {
-            for ($i=1; $i<3; $i++)
-            {
-                if ($i==1) $tempScore = $m_score1;
-                else { $this->score.="-"; $tempScore = $m_score2;}
-                switch($tempScore)
-                {
-                    case 0:
-                        $this->score.="Love";
-                        break;
-                    case 1:
-                        $this->score.="Fifteen";
-                        break;
-                    case 2:
-                        $this->score.="Thirty";
-                        break;
-                    case 3:
-                        $this->score.="Forty";
-                        break;
-                }
-            }
+        $advantagePlayer = $playerOneScore >= 4 || $playerTwoScore >= 4;
+        $deuce = $playerOneScore == $playerTwoScore;
+        if ($deuce) {
+            $this->matchDeuce($playerOneScore);
+        } else if ($advantagePlayer) {
+            $this->matchAdvantage($playerOneScore, $playerTwoScore);
+        } else {
+            $this->matchResult($playerOneScore, $playerTwoScore);
         }
     }
 
     public function __toString()
     {
         return $this->score;
+    }
+
+    public function matchAdvantage($playerOneScore, $playerTwoScore)
+    {
+        $minusResult = $playerOneScore - $playerTwoScore;
+        if ($minusResult == 1) $this->score = "Advantage player1";
+        else if ($minusResult == -1) $this->score = "Advantage player2";
+        else if ($minusResult >= 2) $this->score = "Win for player1";
+        else $this->score = "Win for player2";
+    }
+
+    public function matchDeuce($playerOneScore)
+    {
+        switch ($playerOneScore) {
+            case 0:
+                $this->score = "Love-All";
+                break;
+            case 1:
+                $this->score = "Fifteen-All";
+                break;
+            case 2:
+                $this->score = "Thirty-All";
+                break;
+            case 3:
+                $this->score = "Forty-All";
+                break;
+            default:
+                $this->score = "Deuce";
+                break;
+
+        }
+    }
+
+    public function matchResult($playerOneScore, $playerTwoScore)
+    {
+        for ($i = 1; $i < 3; $i++) {
+            if ($i == 1) $tempScore = $playerOneScore;
+            else {
+                $this->score .= "-";
+                $tempScore = $playerTwoScore;
+            }
+            switch ($tempScore) {
+                case 0:
+                    $this->score .= "Love";
+                    break;
+                case 1:
+                    $this->score .= "Fifteen";
+                    break;
+                case 2:
+                    $this->score .= "Thirty";
+                    break;
+                case 3:
+                    $this->score .= "Forty";
+                    break;
+            }
+        }
     }
 }
